65a602abcbb499c9542884ed4d915d8bad5957e0
hex-plex <sendhilsom7@gmail.com>
2021-01-16 16:40:37 +0530

    script updated

--- gaitdata_fetch.sh
+++ gaitdata_fetch.sh
@@ -1,5 +1,7 @@
 wget -O GaitDatasetB-silh.zip http://www.cbsr.ia.ac.cn/GaitDatasetB-silh.zip
-unzip GaitDataset-silh.zip
-cd GaitDataset-silh
-ls | xargs -1 tar -xf
+unzip GaitDatasetB-silh.zip
+cd GaitDatasetB-silh
+ls | xargs -n 1 tar -xvf
+rm *.tar.gz
+cd ..
 echo "Done"
