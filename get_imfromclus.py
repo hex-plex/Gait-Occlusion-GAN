f9c4357c56968dfbe7deb6c21394c755852a1399
ishikaa25 <ishikaalunawat@gmail.com>
2021-05-28 18:33:24 +0530

    --

--- get_imfromclus.py
+++ get_imfromclus.py
@@ -2,18 +2,18 @@
 from pathlib import Path
 import pickle
 
-def getims(cluster=k,path='/home/ishikaa/Downloads'):
+def getims(cluster=4,path='/home/ishikaa/Downloads'):
     clusters = []
     paths = []
 
     for path in Path(path).rglob('labels.pkl'):
         file_to_read = open(path, "rb")
         temp_dict = pickle.load(file_to_read)
-        temp_cluster = [k for k,v in temp_dict.items() if v==4]
-        temp_path = [path for _,v in temp_dict.items() if v==4]
+        temp_cluster = [k for k,v in temp_dict.items() if v==cluster]
+        temp_path = [path for _,v in temp_dict.items() if v==cluster]
         clusters = clusters + temp_cluster
         paths = paths + temp_path
 
-    paths_k = [str(path).replace('labels.pkl',cluster) for path,cluster in zip(paths4,cluster4)]
+    paths_k = [str(path).replace('labels.pkl',cluster) for path,cluster in zip(paths,clusters)]
 
     return paths_k
